2343c83515ef631c349955cae5d7cad862f2a865
sakazawasaburo <keitaro_nishizawa@gyosei-system.co.jp>
2017-08-25 13:03:52 +0900

    a

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -57,9 +57,9 @@ public class Main {
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      //stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      //stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT * FROM botlog");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
