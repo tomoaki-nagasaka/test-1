6198ec5c81475f9d43cc1b97f8477575d756464a
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-09-08 11:06:25 +0900

    a

--- src/main/java/com/example/AuthController.java
+++ src/main/java/com/example/AuthController.java
@@ -1,1 +1,52 @@
+package com.example;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    UserService userService;
+
+    (snip)
+
+    @GetMapping("/signup")
+    public String signup(Model model) {
+        model.addAttribute("signupForm", new SignupForm());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String signupPost(Model model, @Valid SignupForm signupForm, BindingResult bindingResult, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "signup";
+        }
+
+        try {
+            userService.registerUser(signupForm.getUsername(), signupForm.getPassword(), signupForm.getMailAddress());
+        } catch (DataIntegrityViolationException e) {
+            model.addAttribute("signupError", true);
+            return "signup";
+        }
+
+        try {
+            request.login(signupForm.getUsername(), signupForm.getPassword());
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/messages";
+    }
+
+}
