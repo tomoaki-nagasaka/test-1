79bb3fbe99ec051179e08bfbd7b3087c6036e6c6
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-09-08 15:03:22 +0900

    k

--- src/main/java/com/example/AuthController.java
+++ src/main/java/com/example/AuthController.java
@@ -1,52 +1,86 @@
 package com.example;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
 
 @Controller
-public class AuthController {
+@SpringBootApplication
+public class Main {
 
-    @Autowired
-    UserService userService;
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
 
-    (snip)
+	@Autowired
+	private DataSource dataSource;
 
-    @GetMapping("/signup")
-    public String signup(Model model) {
-        model.addAttribute("signupForm", new SignupForm());
-        return "signup";
-    }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Main.class, args);
+	}
 
-    @PostMapping("/signup")
-    public String signupPost(Model model, @Valid SignupForm signupForm, BindingResult bindingResult, HttpServletRequest request) {
-        if (bindingResult.hasErrors()) {
-            return "signup";
-        }
 
-        try {
-            userService.registerUser(signupForm.getUsername(), signupForm.getPassword(), signupForm.getMailAddress());
-        } catch (DataIntegrityViolationException e) {
-            model.addAttribute("signupError", true);
-            return "signup";
-        }
 
-        try {
-            request.login(signupForm.getUsername(), signupForm.getPassword());
-        } catch (ServletException e) {
-            e.printStackTrace();
-        }
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-        return "redirect:/messages";
-    }
 
+
+		@RequestMapping("/Account")
+		String Account(Map<String, Object> model) {
+			try (Connection connection = dataSource.getConnection()) {
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery("SELECT no,custid,password,authority FROM userdata ORDER BY NO");
+
+				ArrayList<String> output = new ArrayList<String>();
+				while (rs.next()) {
+					output.add(rs.getString("custid") + "  /  " + rs.getString("password"));
+				}
+
+				model.put("records", output);
+				return "db";
+			} catch (Exception e) {
+				model.put("message", e.getMessage());
+				return "error";
+			}
+		}
+
+
+		@Bean
+		public DataSource dataSource() throws SQLException {
+			if (dbUrl == null || dbUrl.isEmpty()) {
+				return new HikariDataSource();
+			} else {
+				HikariConfig config = new HikariConfig();
+				config.setJdbcUrl(dbUrl);
+				return new HikariDataSource(config);
+			}
+		}
+
+	}
 }
