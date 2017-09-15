3d77cb32f1b944c3709a2c634cd07a785c04bbce
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-09-15 13:22:24 +0900

    k

--- src/main/java/com/example/UserRepository.java
+++ src/main/java/com/example/UserRepository.java
@@ -1,4 +1,4 @@
-//package net.teachingprogramming.mybootapp.entity;
+package net.teachingprogramming.mybootapp.entity;
 
 import net.teachingprogramming.mybootapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
