fc1d1e75966dde75c86434dd7a7e45fc53e8e8bc
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-09-19 11:07:33 +0900

    k

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -24,10 +24,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-
 
 import javax.sql.DataSource;
 import java.sql.Connection;
