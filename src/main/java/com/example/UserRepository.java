56b7dd5cc73f88affbf76be5601a3196e004e731
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-09-19 13:28:35 +0900

    j

--- src/main/java/com/example/UserRepository.java
+++ src/main/java/com/example/UserRepository.java
@@ -1,6 +1,6 @@
 package com.example;
 
-import com.example.entity.User;
+import com.example.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 	public interface UserRepository extends JpaRepository<User, String>{
