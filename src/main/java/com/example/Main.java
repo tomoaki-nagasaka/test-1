4548565d16cead29fa281d0d789bdc0b2ce07bf2
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-09-19 10:48:25 +0900

    a

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -35,7 +35,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import com.example.UserRepository
+import com.example.UserRepository;
 
 @Controller
 @SpringBootApplication
