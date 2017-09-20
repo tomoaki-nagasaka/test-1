e3e1e264a136be2ebbfe63b59e2bb7603cf2cd79
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-09-20 09:28:16 +0900

    l

--- src/main/java/com/example/userdata.java
+++ src/main/java/com/example/userdata.java
@@ -1,1 +1,48 @@
+package com.example;
 
+
+public class User {
+
+
+    private String custid;
+    private String username;
+    private String orgname;
+    private String password;
+    //private String role;
+
+    public User() {
+    }
+
+    public String getCustid() {
+        return custid;
+    }
+
+    public void setCustid(String custid) {
+        this.custid = custid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getOrgname() {
+        return orgname;
+    }
+
+    public void setOrgname(String orgname) {
+        this.orgname = orgname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
