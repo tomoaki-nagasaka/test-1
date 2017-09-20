d23fece78fe443b852dee1a4504dcb816effe477
Tomoya_Sakaguchi <Tomoya_Sakaguchi@DESKTOP-GEBSGI4>
2017-09-20 10:40:13 +0900

    a

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -1,1 +1,23 @@
 
+@Autowired
+private UserRepository userRepository;
+@Autowired
+private AuthorityRepository authorityRepository;
+
+@RequestMapping(value = "signup", method = RequestMethod.GET)
+public String signup() {
+    return "signup";
+}
+
+@RequestMapping(value = "signup", method = RequestMethod.POST)
+public String signup(@RequestParam String username, @RequestParam String password) {
+    userdata user = new User();
+    user.setCustid(custid);
+    user.setUsername(username);
+    user.setOrgname(orgname);
+    user.setPassword(password);
+    userRepository.save(user);
+
+    return "signup";
+}
+
