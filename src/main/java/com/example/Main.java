bf83389b7a3dd8b5cd82b08dc0d1ebab7313a8e8
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2017-09-22 16:04:01 +0900

    x

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,10 +2,7 @@ package com.example;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-/*import com.example.userdata;
-import com.example.UserRepository;
-import com.example.WebSecurityConfig;
-*/
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
