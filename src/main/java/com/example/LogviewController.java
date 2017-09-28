5bebd503fa82abcdba1c19c4847f03c6f338388e
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-09-28 19:52:00 +0900

    更新

--- src/main/java/com/example/LogviewController.java
+++ src/main/java/com/example/LogviewController.java
@@ -1,9 +1,21 @@
 package com.example;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Controller
 @SpringBootApplication
@@ -11,8 +23,41 @@ public class LogviewController {
 
 	@RequestMapping("/logview")
 	String logview(Model model) {
-		model.addAttribute("message", "テストですよ");
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM botlog ORDER BY NO");
+
+			HashMap map = new HashMap();
+			ArrayList output = new ArrayList();
+			while (rs.next()) {
+				map.clear();
+				map.put("NO", rs.getString("NO"));
+				map.put("TIME", rs.getString("TIME"));
+				map.put("USERID", rs.getString("USERID"));
+				map.put("CONTENTS", rs.getString("CONTENTS"));
+				map.put("RETURN", rs.getString("RETURN"));
+				output.add(map);
+			}
+
+			model.put("records", output);
+			return "logview";
+		} catch (Exception e) {
+			model.put("message", e.getMessage());
+			return "error";
+		}
+		//model.addAttribute("message", "テストですよ");
 		return "logview";
+	}
+
+	@Bean
+	public DataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
 	}
 
 }
