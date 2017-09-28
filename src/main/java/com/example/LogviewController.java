5772170388265405fc829093498e9a9165d2ec7e
tomoaki nagasaka <tomoaki_nagasaka@gyosei-system.co.jp>
2017-09-28 18:11:41 +0900

    更新

--- src/main/java/com/example/LogviewController.java
+++ src/main/java/com/example/LogviewController.java
@@ -1,6 +1,12 @@
 package com.example;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+@SpringBootApplication
 public class LogviewController {
 
 	@RequestMapping("/logview")
